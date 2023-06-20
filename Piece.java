7ccca79efd3ef08eae16e5791fb620d1becd77dd
Winston Yang <wyang737@users.noreply.github.com>
2023-06-20 00:36:35 -0400

    Add files via upload

--- Piece.java
+++ Piece.java
@@ -2,5 +2,5 @@ import java.util.List;
 
 public interface Piece {
     public List<Coord> getLegalMoves(Board board);
-    public void move(Coord oldCoord, Coord newCoord);
+    public boolean move(Coord newCoord);
 }
