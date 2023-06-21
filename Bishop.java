f0cf60cde237c7b92f4a0d095847a7c0a9a59aac
Charles Chung <charlesgnuhc@gmail.com>
2023-06-21 16:36:59 -0400

    changes to Bishop.java

--- Bishop.java
+++ Bishop.java
@@ -16,49 +16,47 @@ public class Bishop implements Piece {
         List<Coord> ret = new ArrayList<Coord>();
         
         // up and left
-        for (int i = x; i >= 0; i--) {
-            for (int j = y; j <= 7; j++) {
-                if (i-1 >= 0 && y+1 <= 7) {
-                    // check the square at xy
-                    Piece piece = board.getBoard()[i-1][y+1];
-                    if (piece.color != color) { // square open / capturable piece (opponent), piece.color default to -1
-                        ret.add(new Coord(i-1, y+1));
-                    }
-                    else {
-                        // the square occupied by our own piece ->  do nothing
-                    }
+        for (int i = x; i > 0; i--) {
+            for (int j = y; j < 7; j++) {
+                Piece piece = board.getBoard()[i-1][y+1];           // check the square at xy
+                if (piece == null || piece.color != color) {        // square open OR capturable piece (opponent), piece.color default to -1
+                    ret.add(new Coord(i-1, y+1));
+                    break;
                 }
             }
         }
 
         // up and right
-        for (int i = x; i <=7; i++) {
-            for (int j = y; j <= 7; j++) {
-                Piece piece = board.getBoard()[i+1][y+1];
-                if (i+1 <= 7 && y+1 <= 7 && piece.color != color) {
+        for (int i = x; i < 7; i++) {
+            for (int j = y; j < 7; j++) {
+                Piece piece = board.getBoard()[i+1][y+1];           // check the square at xy
+                if (piece == null || piece.color != color) {        // square open OR capturable piece (opponent), piece.color default to -1
                     ret.add(new Coord(i+1, y+1));
+                    break;
                 }
                 
             }
         }
 
         // down and left
-        for (int i = x; i >= 0; i--) {
-            for (int j = y; j >= 0; j--) {
-                Piece piece = board.getBoard()[i-1][y-1];
-                if (i-1 >= 0 && y-1 >= 0 && piece.color != color) {
+        for (int i = x; i > 0; i--) {
+            for (int j = y; j > 0; j--) {
+                Piece piece = board.getBoard()[i-1][y-1];           // check the square at xy
+                if (piece == null || piece.color != color) {        // square open OR capturable piece (opponent), piece.color default to -1
                     ret.add(new Coord(i-1, y-1));
+                    break;
                 }
                 
             }
         }
 
         // down and right
-        for (int i = x; i >=7; i++) {
-            for (int j = y; j >= 0; j--) {
-                Piece piece = board.getBoard()[i+1][y-1];
-                if (i+1 >= 7 && y-1 >= 0 && piece.color != color) {
+        for (int i = x; i > 7; i++) {
+            for (int j = y; j > 0; j--) {
+                Piece piece = board.getBoard()[i+1][y-1];           // check the square at xy
+                if (piece == null || piece.color != color) {        // square open OR capturable piece (opponent), piece.color default to -1
                     ret.add(new Coord(i+1, y-1));
+                    break;
                 }
                 
             }
